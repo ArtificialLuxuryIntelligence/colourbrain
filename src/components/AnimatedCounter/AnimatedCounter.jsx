0887d55d5b93001c97cf07273d0f076c1afcd1d7
Ali <adgk.dev@gmail.com>
2021-01-19 22:56:16 +0000

    animated counted unmount fix

--- src/components/AnimatedCounter/AnimatedCounter.jsx
+++ src/components/AnimatedCounter/AnimatedCounter.jsx
@@ -1,18 +1,24 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 export default function AnimatedCounter({ value = 0, symbol = '%' }) {
   const interval = useRef(null);
+
   const [display, setDisplay] = useState(0);
 
-  interval.current = setInterval(() => {
-    setDisplay((val) => {
-      if (val >= value) {
-        clearInterval(interval.current);
-        return value;
-      }
-      return val + 1;
-    });
-  }, 100);
+  useEffect(() => {
+    interval.current && clearInterval(interval.current);
+
+    interval.current = setInterval(() => {
+      setDisplay((val) => {
+        if (val >= value) {
+          clearInterval(interval.current);
+          return value;
+        }
+        return val + 1;
+      });
+    }, 20);
+    return () => clearInterval(interval.current);
+  }, [value]);
 
   return (
     <span>
@@ -21,3 +27,4 @@ export default function AnimatedCounter({ value = 0, symbol = '%' }) {
     </span>
   );
 }
+// }
